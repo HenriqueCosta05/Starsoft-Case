89abf17ef3dc9de8b15c90979a33b7274737986c
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-11-02 16:01:44 -0300

    feat: animations partially complete

--- src/app/checkout/page.client.tsx
+++ src/app/checkout/page.client.tsx
@@ -1,9 +1,10 @@
 "use client";
-import React from "react";
+import React, {useEffect} from "react";
 import type { Item } from "@/types/Cart";
 import styles from "@/styles/checkout.module.sass";
 import { Arrow, Bin, Coin } from "@/components/Icons";
-import { RedirectComponent, Card, WrapperComponent, ButtonComponent } from "@/components";
+import { RedirectComponent, Card, WrapperComponent, ButtonComponent} from "@/components";
+import { motion } from "framer-motion"
 
 interface PageProps {
     items: Item[];
@@ -13,50 +14,78 @@ interface PageProps {
     totalPrice: number;
 }
 export default function CheckoutClient({items, handleIncrease, handleDecrease, handleDelete, totalPrice}: PageProps) {
+    
   return (
     <main className={styles.base}>
       {items && items.length !== 0 ? (
         <>
-        <section className={styles.intro_section} id="intro">
-        <RedirectComponent>
-          <Arrow />
-        </RedirectComponent>
-        <h1 className={styles.title}>Mochila de Compras</h1>
-      </section>
-      <section className={styles.items_section} id="checkout-items">
-        {items.map((item, index) => (
-          <Card.Root key={index} orientation="horizontal">
-            <Card.Wrapper className={styles.card_image_container}>
-              <Card.Image src={item.image} alt="Product image" />
-            </Card.Wrapper>
-            <Card.Wrapper className={styles.main_content}>
-              <Card.Wrapper className={styles.item_content}>
-                <Card.Title>{item.name}</Card.Title>
-                <Card.Description>{item.description}</Card.Description>
-                <Card.Pricing>{item.price}</Card.Pricing>
-              </Card.Wrapper>
-              <Card.Wrapper className={styles.item_actions}>
-                <Card.Counter amount={item.amount} handleIncrement={() => {handleIncrease(item.id)}} handleDecrement={() => {handleDecrease(item.id)}} />
-                <Card.Toggle onClick={() => {handleDelete(item.id)}}>
-                  <Bin />
-                </Card.Toggle>
-              </Card.Wrapper>
-            </Card.Wrapper>
-          </Card.Root>
-        ))}
-      </section>
-      <WrapperComponent className={styles.finish_order_section}>
-        <WrapperComponent className={styles.total}>
-          <h1>Total</h1>
-          <span className={styles.price}>
-            <Coin />
-            <h2>{totalPrice !== undefined ? totalPrice : 0} ETH</h2>
-          </span>
-        </WrapperComponent>
-        <ButtonComponent className={styles.finish_order_button}>Finalizar Compra</ButtonComponent>
-      </WrapperComponent>
-      </>
-      ): (
+          <motion.section
+            className={styles.intro_section}
+            id="intro"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <RedirectComponent>
+              <Arrow />
+            </RedirectComponent>
+            <h1 className={styles.title}>Mochila de Compras</h1>
+          </motion.section>
+          <motion.section className={styles.items_section} id="checkout-items">
+            {items.map((item, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.3, delay: index * 0.1 }}
+              >
+              <Card.Root key={index} orientation="horizontal">
+                <Card.Wrapper className={styles.card_image_container}>
+                  <Card.Image src={item.image} alt="Product image" />
+                </Card.Wrapper>
+                <Card.Wrapper className={styles.main_content}>
+                  <Card.Wrapper className={styles.item_content}>
+                    <Card.Title>{item.name}</Card.Title>
+                    <Card.Description>{item.description}</Card.Description>
+                    <Card.Pricing>{item.price}</Card.Pricing>
+                  </Card.Wrapper>
+                  <Card.Wrapper className={styles.item_actions}>
+                    <Card.Counter
+                      amount={item.amount}
+                      handleIncrement={() => {
+                        handleIncrease(item.id);
+                      }}
+                      handleDecrement={() => {
+                        handleDecrease(item.id);
+                      }}
+                    />
+                    <Card.Toggle
+                      onClick={() => {
+                        handleDelete(item.id);
+                      }}
+                    >
+                      <Bin />
+                    </Card.Toggle>
+                  </Card.Wrapper>
+                </Card.Wrapper>
+              </Card.Root>
+              </motion.div>
+            ))}
+          </motion.section>
+          <WrapperComponent className={styles.finish_order_section}>
+            <WrapperComponent className={styles.total}>
+              <h1>Total</h1>
+              <span className={styles.price}>
+                <Coin />
+                <h2>{totalPrice !== undefined ? totalPrice : 0} ETH</h2>
+              </span>
+            </WrapperComponent>
+            <ButtonComponent className={styles.finish_order_button}>
+              Finalizar Compra
+            </ButtonComponent>
+          </WrapperComponent>
+          </>
+      ) : (
         <section className={styles.empty_section}>
           <h1>Ainda não existem items no carrinho!</h1>
           <RedirectComponent>
@@ -64,7 +93,6 @@ export default function CheckoutClient({items, handleIncrease, handleDecrease, h
           </RedirectComponent>
         </section>
       )}
-      
     </main>
   );
 }
--- src/components/Card/Root.tsx
+++ src/components/Card/Root.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import styles from '@/styles/Card/Root.module.sass'
+import React, { useRef } from "react";
+import styles from "@/styles/Card/Root.module.sass";
+import { motion, useInView } from "framer-motion";
 
 interface RootProps {
-  children: React.ReactNode
-  orientation: 'vertical' | 'horizontal'
+  children: React.ReactNode;
+  orientation: "vertical" | "horizontal";
 }
-export default function RootComponent({children, orientation}: RootProps) {
+
+export default function RootComponent({ children, orientation }: RootProps) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { triggerOnce: false });
+
   return (
-      <div className={orientation === 'horizontal' ? `${styles.item_wrapper}` : `${styles.card_wrapper}`}>
-          {children}
-    </div>
-  )
+    <motion.div
+      ref={ref}
+      initial={{ y: 100, opacity: 0 }}
+      animate={isInView ? { y: 0, opacity: 1 } : { y: 100, opacity: 0 }}
+      transition={{ duration: 0.5 }}
+      className={
+        orientation === "horizontal"
+          ? `${styles.item_wrapper}`
+          : `${styles.card_wrapper}`
+      }
+    >
+      {children}
+    </motion.div>
+  );
 }
